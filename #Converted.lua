b5c52e579412fc9e29afc0b01ede0979f1263d71
Zegion <138577338+VerzIsHere@users.noreply.github.com>
2025-09-13 15:38:27 +0700

    Add files via upload

---
+++ #Converted.lua
@@ -0,0 +1,11 @@
+[🛡] Encoded with PG LunarCrypt [🛡]
+
+What is PG LunarCrypt?, PG LunarCrypt is a online lua encode tools which can encrypt any type of lua script without errors!
+Super secure method for encrypting online script, especially if we need raw type of lua script. It doesn't matter if our raw link script got leak, they just can't do anything about this.
+Why they can't do anything?, it's because PG LunarCrypt was based on 'end-to-end' algorithms. So no one will never figure it out until they got the decode formula.
+
+[📌] Join Our Telegram [📌]
+➣ https://t.me/PGTzCh
+➣ https://t.me/PGGrupChat
+
+🇮🇩[SᏔ • 𝐏𝐆 • SHK] Not4Hoes-lcDbHFT2RHc-q<xMQ+whRvfvAK/lVNsTW/rRYraJgpTb-c-VxYh2N9=_Vfu2WhY33YjN8bRDbhWRvjG9BbFTVwzK17bNIPbuThWH6>GrPbuGARw9DWF/>fB9NqbsGVfXjC0C5mqIAqwsORMNYczcfm8#acG2Uzgps0q/L>OeeZO-3ZcJ6rumlH-dhc/I1Hf9fExUuUjQOxvsl8_HbqPlVHYrRNN#8crXcGZs9jvd2R/_efFN1qNJZcaNCqUIWD_K-Of<f8wzD-f_Olc1V1XsGVXebc-FYqNuWHYs7N_GZfN3EZRNacGf2R/_ef/wcbKmEcGlhZMc6rurrbG1#SBe71RumPHmTRjs/VE17lNmdqNuWHlFhRu/bcf_0cBQdNR-fblh1YFGvqHTecGlhZMRCAcD5OHTh8R1cYf_VburK>/dgwvh2fHFSRcufHH_cN_e#cf_0m8#acGf2R/_e1Yw#eorIjGzMZMc6rumlOH1Tc/26Yf9YOc1VYN-GVXebeKGwN_u7HlfmAc_2lYrfhBQdNR-fblhe1-wjPF2H8cImmMucrumlONNxNuRwjb#PNK-fbMTaxjuKmb3+eve#1WrU7Oc0ZM/1ZRNacGf2Nls8RfwTb_cfZl-wAqf#7ulqW/F7fcKHbGeOW/zwcO-gZomcculYYu#DDP-#rarGZ8TV-82mcGf2RFQ7P-mDY_J8lMJehBDC1-<8q/RSNRTWq/hoe8rzmuzrw-rvjPDYqNuWw/dqfN_8H9dEjBQ+NNcpO/#hlN3VOzFhq/de>OJ_ezeOOGdhjzNpSbHU78uBeHlfOX<#c-FYqH<cYGJTNcc2lOD6SW9BHHzV-G#>PFlAYuGIVoNQ-HmqOb2/Rq/h8R1cqFQmb_1-7B<UAvsqHGRfO9cwPODhRu/blFs9hWIifXNNb/N>NcGpwH17l-J3wzRxWbcdrMh3l8ucYf9Yb_wVPve9pUHKllsjffzHPlwMRu/bclQ#7BI3NFrhO/<vF6g#UO#3NRcRpqw+D_Dl>FrAOzm8OFJjNcJDqXhVxvuPSvhl8-e-Rf-Wb-uzmjFN--J5jvd2R/_e-f1jbKrDeNGZDBDvDG#wY9rTl8ucYf9YN9fVYWTdAXdZSKGmYb1QwGGqllGZ8Fh0Rv9Bl__hffNH>RN1qH17l-J2hPw<Tu<853JLcGTmO9HNlRDNPEhGgLx6iCbPo7Ug9eU2CroqLIBLXO5XL1Xe6TZ2+hxTQ3d>jv#ThqNcbR-mlc3T8-ebjR/Ybc/iqbsGVB#Hcc3NwGRHDz-Z8_sVlwlLTBQBjuHh8F/8>RN1qHTecGlhqqf#p-m1OfDM8R1cYf9YO-rYYMfUxU1ublsVPGfjPf#DbFJYjWr0m8#alKsrAw_efFlwYG1LlFrbZoGEr/elPNsTcoucYf9YO-mNqfQ8OEG0NN-me-_M28mJxXrM2jhcN9-PhU3HN_lmq----q/ecGlhSB<gbc1>bP/w
